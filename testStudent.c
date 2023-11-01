d26cfdc4cf9a80f79bd947965837bba5f7af4706
Syahmi Samri <68218569+Voidstorm012@users.noreply.github.com>
2023-11-01 16:20:44 +0800

    Add files via upload

--- testStudent.c
+++ testStudent.c
@@ -260,9 +260,9 @@ int getBlinkBrightness()
 }
 
 /*
-Menu to get user selction on Frequency to blink
+Menu to get user selction on duty cycle
 */
-int getBlinkDutyCycle()
+int getBlinkDutyCycle()                                                     // Added another menu selection for user to input duty cycle
 {
     int selection;
 
@@ -301,7 +301,7 @@ int confirmBlinkSelection(int blinkLed, int blinkFrequency, int blinkDutyCycle, 
     printf("Confirm your blink configrations.\n\n");
     printf("LED to blink: %s\n", blinkLedString);
     printf("Blink Frequency: %dHz\n", blinkFrequency);
-    printf("Blink Duty Cycle: %d%%\n\n", blinkDutyCycle);
+    printf("Blink Duty Cycle: %d%%\n", blinkDutyCycle);                     // Added another menu selection for user to input duty cycle
     printf("Blink Brightness: %d%%\n\n", blinkBrightness);
     printf("[1] Confirm Configuration\n");
     printf("[0] Return to Home\n");
@@ -394,7 +394,7 @@ void blinkLedWithConfig(int blinkLed, int blinkFrequency, int blinkDutyCycle, in
     int **dataArray;                                                        // Setting up the array
     int cols = 2;
     int rows = 1;
-    dataArray = malloc(sizeof(int *) * rows);
+    dataArray = malloc(sizeof(int *) * rows);                               // Allocating memory to the array
     for (int i = 0; i < rows; i++)
     {
         dataArray[i] = malloc(sizeof(int) * cols);
@@ -403,15 +403,15 @@ void blinkLedWithConfig(int blinkLed, int blinkFrequency, int blinkDutyCycle, in
     printf("\nBlinking...\n");
     
     // Setting Frequency
-    float dutyCycle = blinkDutyCycle * 10;
-    float onTime = 1.0f / blinkFrequency * dutyCycle;
-    float offTime = 1.0f / blinkFrequency * (1000 - dutyCycle);
+    float dutyCycle = blinkDutyCycle * 10;                                  // Calculate the duty cycle from user input
+    float onTime = 1.0f / blinkFrequency * dutyCycle;                       // Time for LED to be in HIGH state depending on duty cycle
+    float offTime = 1.0f / blinkFrequency * (1000 - dutyCycle);             // Time for LED to be in LOW state depending on duty cycle
     unsigned long theTime = 0;
     unsigned long previousTime = 0;
-    int loopDuration = 60 * blinkFrequency;
+    int loopDuration = 120 * blinkFrequency;
           
     // Setting Blink LED
-    char blinkLedString[] = "Green";  
+    char blinkLedString[] = "Green";
     if (blinkLed == BLINK_GREEN)
     {
         blinkLed = GREEN;
@@ -426,12 +426,11 @@ void blinkLedWithConfig(int blinkLed, int blinkFrequency, int blinkDutyCycle, in
     unsigned long previousMillis = 0;
     int ledState = LOW;
 
-
     for (int blink = 0; blink < loopDuration;)
     {
-        int interval = 20;                                                  // 20 milliseconds                              (Linked to the usleep below)
+        int interval = 20;                                                  // 20 milliseconds (To save data every 20ms)  (Linked to the usleep below)
         
-        while (blink < loopDuration)                                                  // Timer
+        while (blink < loopDuration)                                        // Timer
         {
             if (previousMillis != 0) {                                      // This is to get the timing intervals between each iteration to be saved in CSV
                 unsigned long currentTime = millis();
@@ -487,17 +486,16 @@ void blinkLedWithConfig(int blinkLed, int blinkFrequency, int blinkDutyCycle, in
     // Save to CSV
     FILE *saveFile = fopen("waveformData.csv", "a");                        // Open/Create file
     fprintf(saveFile, "START\n");
-    fprintf(saveFile, "%s LED. Running at %dHz frequency with a duty cycle of %d%%. Brightness set to %d%%.\n\n", blinkLedString, blinkFrequency, blinkDutyCycle, blinkBrightness);
+    fprintf(saveFile, "%s LED. Running at %dHz frequency with a duty cycle of %d%%. Brightness set to %d%%.\n", blinkLedString, blinkFrequency, blinkDutyCycle, blinkBrightness);
     for (int i = 0; i < rows-1; i++)
     {
         fprintf(saveFile, "%d, %d\n", dataArray[i][0], dataArray[i][1]);    // Save from array
     }
-    fprintf(saveFile, "END\n\n");
+    fprintf(saveFile, "END\n");
 
     fclose(saveFile);
-    printf("Data saved to waveformData.csv\n");
 
-    // Free Memory from dataArray to prevent memory leak
+    // Free memory from dataArray to prevent memory leak (Reduce wastage of memory)
     for (int i = 0; i < rows; i++)
     {
         free(dataArray[i]);
